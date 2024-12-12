814b8e3c05c3db7b8aa7da948fff1188f4fb5713
wds <awdx268412@268412>
2024-12-12 13:23:32 +0800

    [any message]

--- src/Server.cs
+++ src/Server.cs
@@ -17,18 +17,22 @@ Console.WriteLine($"Server listenting on port {port}");
 while (true)    // 讓 server 一直跑處理多個連接
 {
     // 3. 等待並接收 client 端連接
-    using (Socket socket = server.AcceptSocket())
+    using (Socket socket = server.AcceptSocket())   // TCP 握手(SYN-SYN+ACK-ACK)
     {
         Console.WriteLine("client 成功連接");
 
         // 4. 建立 HTTP 回應字串
         string responeString = "HTTP/1.1 200 OK\r\n\r\n";
-        byte[] responseBytes = Encoding.UTF8.GetBytes(responeString);   
+        byte[] responseBytes = Encoding.UTF8.GetBytes(responeString);
 
         // 5. 發送 HTTP 回應給client端
+        socket.Send(responseBytes);
+        Console.WriteLine("已傳送response");
+
+        // 6. 關閉client 連線(這邊只處理單一request)
+        socket.Close();
     }
 }
-//var socket = server.AcceptSocket(); // wait for client
 
 
 
